ee5358e4e028d0dcf26c80162f87bd858575b5ff
Kieron Browne <kieron.browne@gmail.com>
2012-10-18 22:45:30 +0100

    Added bootstrap layout with dummy navbar

--- controllers/index.js
+++ controllers/index.js
@@ -6,12 +6,5 @@
 var Recipe = require('../models/recipe');
 
 exports.index = function(req, res){
-  var toast = new Recipe({name: 'Toast'});
-  toast.portions = 1;
-  console.dir(toast);
-  toast.save(function(err) {
-	  if(err) console.log('oops');
-  });
-  toast.setName('Desire');
-  res.render('index', { title: 'Express', recipe: toast });
+  res.render('index', { title: 'Meal Planner' });
 };
--- controllers/recipe.js
+++ controllers/recipe.js
@@ -3,21 +3,21 @@ var Recipe = require('../models/recipe');
 exports.list = function(req, res){
 	var recipes = Recipe.find({}, function(err, recipes) {
 		if (err) return console.log(err);
-		res.render('recipe/list', {recipes: recipes});
+		res.render('recipe/list', {recipes: recipes, title: 'Recipes'});
 	});
 };
 
 exports.show = function(req, res) {
 	var id = req.params.id;
 	Recipe.findOne({_id: id}, function(err, recipe) {
-		res.render('recipe/show', {recipe: recipe});
+		res.render('recipe/show', {recipe: recipe, title: 'Recipes'});
 	});
 };
 
 exports.edit = function(req, res) {
 	var id = req.params.id;
 	Recipe.findOne({_id: id}, function(err, recipe) {
-		res.render('recipe/edit', {recipe: recipe});
+		res.render('recipe/edit', {recipe: recipe, title: 'Recipes'});
 	});
 };
 
